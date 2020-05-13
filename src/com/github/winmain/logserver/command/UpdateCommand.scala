4990a243b9f237cfb70e3026789d17ce604846f0
Denis Denisenko <denis@eidoloncorp.com>
2020-05-13 15:02:49 +0300

    Minor fixes

--- src/com/github/winmain/logserver/command/UpdateCommand.scala
+++ src/com/github/winmain/logserver/command/UpdateCommand.scala
@@ -8,11 +8,11 @@ case class UpdateCommand() extends Command {
   /**
    * Добавить новые данные в базу и удалить успешно прочитанные данные логов.
    * Например: dbDir = /mnt/test/logs/2015, updates = /home/myproject/log/sql-new
-   * dbDir - Путь до базы данных логов
+   * dbDir - Путь до базы данных логов с годом вконце
    * updates - Обновления - каталог с обновлениями, либо сами файлы обновлений
    */
   override def run(log: Logger, params: Array[String]): Unit = {
-    if (params.length < 2) exitError("Usage: convert-month <db-dir> <updates>")
+    if (params.length < 2) exitError("Usage: update <db-dir> <updates>")
 
     val dbDir = Paths.get(params(0))
     val updatePaths: Vector[Path] = params.drop(1).map(Paths.get(_))(collection.breakOut)
